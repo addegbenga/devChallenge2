29dd26d1bc45edf23d60cfc962776d07fbe61e2e
addegbenga <addegbenga@gmail.com>
2021-10-10 17:33:01 -0400

    changes

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,20 +1,20 @@
-const {createProxyMiddleware} = require("http-proxy-middleware");
+// const {createProxyMiddleware} = require("http-proxy-middleware");
 
-module.exports = function (app) {
-  app.use(
-    "/giphy",
-    createProxyMiddleware({
-      target: "https://www.metaweather.com/api",
-      changeOrigin: true,
-    }),
-  );
-  app.use(
-    "/ip",
-    createProxyMiddleware({
-      target: "https://extreme-ip-lookup.com/json",
-      changeOrigin: true,
-    }),
-  );
-};
+// module.exports = function (app) {
+//   app.use(
+//     "/giphy",
+//     createProxyMiddleware({
+//       target: "https://www.metaweather.com/api",
+//       changeOrigin: true,
+//     }),
+//   );
+//   app.use(
+//     "/ip",
+//     createProxyMiddleware({
+//       target: "https://extreme-ip-lookup.com/json",
+//       changeOrigin: true,
+//     }),
+//   );
+// };
 
 
