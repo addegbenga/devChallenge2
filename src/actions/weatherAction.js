f1671da8cb913bad53859ca844a82b9680399e6e
addegbenga <addegbenga@gmail.com>
2021-10-10 11:23:01 -0400

    changes

--- src/actions/weatherAction.js
+++ src/actions/weatherAction.js
@@ -14,7 +14,7 @@ export const getWeatherByLocation = (body) => async (dispatch) => {
       type: IS_LOADING,
     });
     const response = await axios.get(
-      `/api/location/search/?lattlong=${body.lat},${body.long}`
+      `api/location/search/?lattlong=${body.lat},${body.long}`
     );
     console.log(response);
     // /api/location/search/?lattlong=36.96,-122.02
@@ -36,7 +36,7 @@ export const getCurrentLocationWeather = (woeid) => async (dispatch) => {
       type: IS_LOADING,
     });
 
-    const response = await axios.get(`/api/location/${woeid}/`);
+    const response = await axios.get(`api/location/${woeid}/`);
     dispatch({
       type: GET_CURRENT_WEATHER,
       payload: response.data,
@@ -55,7 +55,7 @@ export const getWeatherByQuery = (query) => async (dispatch) => {
       type: IS_LOADING,
     });
 
-    const response = await axios.get(`/api/location/search/?query=${query}`);
+    const response = await axios.get(`api/location/search/?query=${query}`);
     dispatch({
       type: GET_WEATHER_QUERY_RESULT,
       payload: response.data,
