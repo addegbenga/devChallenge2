5173547755623da65b054dcca98dd87eedcb92d6
addegbenga <addegbenga@gmail.com>
2021-10-10 08:03:17 -0400

    changes

--- src/actions/weatherAction.js
+++ src/actions/weatherAction.js
@@ -7,7 +7,6 @@ import {
   GET_WEATHER_ERROR,
 } from "../actions/types";
 
-const api = "https://www.metaweather.com/";
 
 export const getWeatherByLocation = (body) => async (dispatch) => {
   try {
@@ -16,7 +15,9 @@ export const getWeatherByLocation = (body) => async (dispatch) => {
       type: IS_LOADING,
     });
 
-    const response = await axios.get(`${api}${body.lat},${body.long}`);
+    const response = await axios.get(
+      `api/location/search/?lattlong=${body.lat},${body.long}`
+    );
     // /api/location/search/?lattlong=36.96,-122.02
     dispatch({
       type: GET_WEATHER,
@@ -36,7 +37,7 @@ export const getCurrentLocationWeather = (woeid) => async (dispatch) => {
       type: IS_LOADING,
     });
 
-    const response = await axios.get(`${api}${woeid}/`);
+    const response = await axios.get(`api/location/${woeid}/`);
     dispatch({
       type: GET_CURRENT_WEATHER,
       payload: response.data,
@@ -55,7 +56,7 @@ export const getWeatherByQuery = (query) => async (dispatch) => {
       type: IS_LOADING,
     });
 
-    const response = await axios.get(`${api}${query}`);
+    const response = await axios.get(`/api/location/search/?query=${query}`);
     dispatch({
       type: GET_WEATHER_QUERY_RESULT,
       payload: response.data,
